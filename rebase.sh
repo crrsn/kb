8d14729851e01d1282089342bdc0ede2f319a53a
crrsn <jn3617@yahoo.com.tw>
2022-06-11 18:14:43 +0800

    add rebase.sh file

---
+++ rebase.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# source: https://stackoverflow.com/questions/11929766/how-to-delete-all-git-commits-except-the-last-five/11931881
+
+# https://gist.github.com/csweichel/cd75135533fbfcba81b278c00ab2343b
+
+b="$(git branch --no-color | cut -c3-)" 
+h="$(git rev-parse $b)" 
+echo "Current branch: $b $h" 
+c="$(git rev-parse $b~4)" 
+echo "Recreating $b branch with initial commit $c ..." 
+git checkout --orphan new-start $c 
+git commit -C $c 
+git rebase --onto new-start $c $b 
+git branch -d new-start 
+git gc
